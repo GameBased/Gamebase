5f3a62e1cdbde5e01a6e11fad3c93df2a5aebb4a
Galdin Raphael <gldraphael@gmail.com>
2017-03-10 05:17:49 +0530

    Create a TimerBase class

---
+++ utils/TimerBase.cpp
@@ -0,0 +1,48 @@
+#include "TimerBase.h"
+
+TimerBase::TimerBase()
+    : startTicks(0), stopTicks(0), _isRunning(false)
+{
+    
+}
+
+void TimerBase::Start()
+{
+    _isRunning = true;
+    startTicks = getTicks();
+}
+
+void TimerBase::Stop()
+{
+    if(!isRunning())
+    {
+        // Already stopped
+        return;
+    }
+    _isRunning = false;
+    stopTicks = getTicks();
+}
+
+void TimerBase::Reset()
+{
+    _isRunning = false;
+    startTicks = 0;
+    stopTicks = 0;
+}
+
+unsigned int TimerBase::getElapsedTime()
+{
+    if(isRunning())
+    {
+        return getTicks() - startTicks;
+    }
+    else
+    {
+        return stopTicks - startTicks;
+    }
+}
+
+bool TimerBase::isRunning()
+{
+    return _isRunning;
+}
